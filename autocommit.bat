2d4a3639bc2c2c4874804f05aad1525574ed086d
markus <mafonso4@my.bcit.ca>
2024-03-20 22:12:20 -0700

    // 2024-03-20 22:12:20.85

--- autocommit.bat
+++ autocommit.bat
@@ -1,7 +1,17 @@
+@echo off
+setlocal
 
+:try
 set /p message=Enter a message:
 
 git pull origin main
 git add .
 git commit -m "%message% // %DATE% %TIME%"
 git push origin main
+
+goto :eof
+
+:catch
+echo An error occurred.
+pause
+exit /b 1
